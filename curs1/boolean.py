66075a6682993e3c3d5630eeae0a4a7285959d29
sojog <sil.ojog@gmail.com>
2023-03-20 10:47:23 +0200

    Curs 1 - boolean usage

---
+++ curs1/boolean.py
@@ -0,0 +1,21 @@
+''' Folosirea intrebarii in variabilele booleane '''
+isSad = False
+
+
+if isSad ==  True:
+    print('adevarat')
+
+if isSad:
+    print('adevarat')
+else:
+    print('false')
+
+
+if isSad ==  False:
+    print('adevarat')
+
+if not isSad:
+    print('adevarat')
+
+# Operator ternar
+print('adevarat') if isSad else  print('false')
