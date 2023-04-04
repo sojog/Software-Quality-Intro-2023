a5f5cb2e2cbd3f410001afb33f41ea8f6412656b
sojog <sil.ojog@gmail.com>
2023-04-04 09:46:10 +0300

    Suplimentar 2 - while loop

---
+++ suplimentar_clasa/2.while.py
@@ -0,0 +1,19 @@
+i = 1
+print("i=", i)
+while i < 6:
+    print("i=", i)
+    i += 1
+    print("i=", i)
+    if i == 3 :
+        print("i=", i)
+        continue
+    print('PARTE DIN BUCLA')    
+    print('PARTE DIN BUCLA')    
+
+else:
+    print("i=", i)
+    i+=1
+
+print("i=", i)
+i += 1
+print("i=", i)
